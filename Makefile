64ca546cdf6a146fbd3a8b26313e82039b764d5a
Christopher J. Brody <brodybits@litehelpers.net>
2018-01-09 12:10:26 -0500

    general build fixes

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
+# top-level Makefile
 
 all: ndkbuild
 
@@ -8,13 +9,11 @@ regen:
 	java -cp gluegentools/antlr.jar:gluegentools/gluegen.jar com.jogamp.gluegen.GlueGen -I. -Ecom.jogamp.gluegen.JavaEmitter -CEVCoreNativeDriver.cfg native/sqlc.h
 	sed -i.orig 's/^import/\/\/import/' java/io/sqlc/EVCoreNativeDriver.java
 
-# NOTE: adding v (verbose) flag for the beginning stage:
 ndkbuild:
 	rm -rf lib libs
 	ndk-build
 	cp -r libs lib
 	jar cf evcore-native-driver.jar lib
-
 
 clean:
 	rm -rf obj lib libs *.jar *.zip *.jar
--- jni/Android.mk
+++ jni/Android.mk
@@ -1,6 +1,12 @@
-# see http://mobile.tutsplus.com/tutorials/android/ndk-tutorial/
+# THANKS for INITIAL GUIDANCE:
+# https://code.tutsplus.com/tutorials/advanced-android-getting-started-with-the-ndk--mobile-2152
+# original location:
+# http://mobile.tutsplus.com/tutorials/android/ndk-tutorial/
 
 LOCAL_PATH := $(call my-dir)
+
+ROOT_PATH := $(LOCAL_PATH)/..
+SRC_PATH := ../native
 
 include $(CLEAR_VARS)  
 
@@ -8,20 +14,24 @@ LOCAL_LDLIBS := -llog
 
 LOCAL_MODULE    := sqlc-evcore-native-driver
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../sqlite-amalgamation
+LOCAL_C_INCLUDES := $(ROOT_PATH)/sqlite-amalgamation
+
+# extbuild only:
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../sqlite3-regexp-cached
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../libb64-encode
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../sqlite3-base64
 
 LOCAL_CFLAGS += -DSQLITE_TEMP_STORE=2 -DSQLITE_THREADSAFE=2
 LOCAL_CFLAGS += -DSQLITE_ENABLE_FTS3 -DSQLITE_ENABLE_FTS3_PARENTHESIS -DSQLITE_ENABLE_FTS4 -DSQLITE_ENABLE_RTREE
+
+# extbuild only:
 LOCAL_CFLAGS += -DSQLITE_ENABLE_FTS5
 LOCAL_CFLAGS += -DSQLITE_ENABLE_JSON1
 
+# additional flags:
 LOCAL_CFLAGS += -DSQLITE_DEFAULT_PAGE_SIZE=4096
 LOCAL_CFLAGS += -DSQLITE_DEFAULT_CACHE_SIZE=-2000
 
-LOCAL_SRC_FILES := ../native/sqlc_all.c
+LOCAL_SRC_FILES := $(SRC_PATH)/sqlc_all.c
 
 include $(BUILD_SHARED_LIBRARY)
-
