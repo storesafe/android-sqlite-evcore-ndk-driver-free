073d21c31414bf917cfd646413d55822bba36e8c
Christopher J. Brody <brodybits@litehelpers.net>
2018-04-05 20:15:12 -0400

    jni/Application.mk cleanup & mark TODO

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,5 +1,6 @@
-#APP_ABI := all
+# Includes DEPRECATED platforms (armeabi mips mips64):
+# APP_ABI := all
+# [TBD] Includes DEPRECATED armeabi platform:
 APP_ABI := armeabi armeabi-v7a x86 x86_64 arm64-v8a
-# For future consideration (needs testing):
-#APP_ABI += mips mips64
-
+# FUTURE (TODO) explicit for Android 4.0 and greater:
+# APP_PLATFORM := android-14
